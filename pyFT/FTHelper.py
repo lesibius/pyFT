ebed7f3d84acb045907ebdab2181aaa350a7744e
lesibius <clementin.castellano@gmail.com>
2017-08-22 00:09:27 +0200

    Started working on FTHelper module (wrapper for search query refinement)

---
+++ pyFT/FTHelper.py
@@ -0,0 +1,14 @@
+import pyFT.Request
+import pyFT.FTError
+
+
+facets = ["authors","authorsId","brand","brandId","category","format","genre","genreId","icb","icbId","iptc","iptcId","organisations","organisationsId","people","peopleId","primarySection","primarySectionId","primaryTheme","primaryThemeId","regions","regionsId","sections","sectionsId","specialReports","specialReportsId","subjects","subjectsId","topics","topicsId"]
+
+class FTFacets:
+    """
+    Helper for facets
+    """
+    def __init__(self):
+        raise NotImplementedError('Not implemented')
+
+
--- pyFT/__init__.py
+++ pyFT/__init__.py
@@ -2,5 +2,6 @@ from pyFT.Request import *
 from pyFT.Result import *
 from pyFT.FTQuerySyntax import *
 from pyFT.FTError import *
+import pyFT.FTHelper as FTHelper
 
 
