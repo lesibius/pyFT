326c3806dc8196afdf666a046558b112dce54dc5
lesibius <clementin.castellano@gmail.com>
2017-08-21 23:16:20 +0200

    Added FTSearch in syntax

--- pyFT/FTQuerySyntax.py
+++ pyFT/FTQuerySyntax.py
@@ -1,7 +1,10 @@
+import pyFT
+
+searchable = ["apiUrl","authors","authorsId","brand","brandId","byline","category","contributorRights","format","genre","genreId","icb","icbId","id","initialPublishDateTime","iptc","iptcId","lastPublishDateTime","masterEntityId","masterSource","organisations","organisationsId","originatingParty","people","peopleId","primarySection","primarySectionId","primaryTheme","primaryThemeId","regions","regionsId","runtimeMilliseconds","sections","sectionsId","specialReports","specialReportsId","subheading","subjects","subjectsId","title","topics","topicsId","uri"]
+
 class FTBasic:
     def __init__(self,val):
         self.val = val
-
     def __add__(self,other):
         return FTOR(self,other)
     def __radd__(self,other):
@@ -20,6 +23,41 @@ class FTBasic:
     def evaluate(self):
         return self.val
 
+class FTSearch(FTBasic):
+    """
+    """
+    def __init__(self,field,search,equals=False):
+        """
+        Parameters
+        ----------
+        field: Field searched (must be searchable)
+        search: Term searched
+        equals: (Optional) If True, the field must match, else it should contain the search term
+        """
+        FTBasic.__init__(self,'')
+        self.update(field,search,equals)
+        
+
+    def _makeString(self):
+        _ = self.field + ":"
+        if self.equals:
+            _ += "="
+        _ += "\"" + self.search + "\""
+        return _
+
+    def update(self,field,search,equals):
+        if not field in searchable:
+            raise pyFT.FTError.FTException("Field \"" + field + "\" not searchable")
+        self.field = field
+        self.search = search
+        self.equals = equals
+        _ = self._makeString()
+        self.val = _
+        
+                
+
+                    
+    
 class FTNil(FTBasic):
     def __init__(self):
         FTBasic.__init__(self,"")
